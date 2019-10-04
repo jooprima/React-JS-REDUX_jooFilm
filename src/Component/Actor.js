041d1f5b68561db53614e4fe697b0a0fd852b298
jooprima <jooprima@gmail.com>
2019-10-05 02:19:15 +0700

    create page actor part 2

--- src/Component/Actor.js
+++ src/Component/Actor.js
@@ -2,32 +2,74 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Carousel } from "react-responsive-carousel";
 import { Grid, Card, Input, Icon } from "semantic-ui-react";
-
-var Images = [
-  "https://cdn.pixabay.com/photo/2015/03/26/09/43/lenses-690179__480.jpg",
-  "https://cdn.pixabay.com/photo/2019/04/24/21/55/cinema-4153289__480.jpg",
-  "https://cdn.pixabay.com/photo/2014/10/31/17/41/dancing-dave-minion-510835__480.jpg",
-  "https://cdn.pixabay.com/photo/2016/01/22/08/17/banner-1155437__480.png",
-  "https://cdn.pixabay.com/photo/2019/01/13/21/36/analog-3931362__480.jpg",
-  "https://cdn.pixabay.com/photo/2017/12/18/13/03/grain-3026099__480.jpg",
-  "https://cdn.pixabay.com/photo/2015/05/15/09/13/demonstration-767982__480.jpg",
-  "https://cdn.pixabay.com/photo/2016/11/15/07/09/photo-manipulation-1825450__480.jpg"
-];
+import Axios from "axios";
 
 class Actor extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataFilm: [],
+      dataActor: []
+    };
+  }
+
+  getDataFilm = () => {
+    Axios.get(`http://api.tvmaze.com/search/shows?q=d`).then(res => {
+      this.setState({
+        dataFilm: res.data
+      });
+    });
+  };
+
+  getDataActor = () => {
+    Axios.get(`http://api.tvmaze.com/search/people?q=b`).then(res => {
+      this.setState({
+        dataActor: res.data
+      });
+    });
+  };
+
+  pencarianActor = e => {
+    if (e.target.value === "") {
+      this.getDataActor();
+    } else {
+      Axios.get(`http://api.tvmaze.com/search/people?q=${e.target.value}`).then(
+        res => {
+          this.setState({
+            dataActor: res.data
+          });
+        }
+      );
+    }
+  };
+
+  componentDidMount() {
+    this.getDataFilm();
+    this.getDataActor();
+  }
+
   render() {
     return (
       <div>
         <Carousel
           autoPlay
           centerMode
-          centerSlidePercentage={40}
+          centerSlidePercentage={20}
           showStatus={false}
         >
-          {Images.map((data, key) => {
+          {this.state.dataFilm.map((data, key) => {
+            var images = { ...data.show.image };
+
+            if (data.show.images === null) {
+              images =
+                "https://cdn.pixabay.com/photo/2019/01/13/21/36/analog-3931362__480.jpg";
+            } else {
+              images = images.original;
+            }
+
             return (
               <div key={key}>
-                <img src={data} alt="" />
+                <img src={images} alt="" />
               </div>
             );
           })}
@@ -41,35 +83,58 @@ class Actor extends Component {
               centerSlidePercentage={40}
               showStatus={false}
             >
-              {Images.map((data, key) => {
+              {this.state.dataActor.map((data, key) => {
+                var images = { ...data.person.image };
+
+                if (data.person.image === null) {
+                  images =
+                    "https://cdn.pixabay.com/photo/2019/01/13/21/36/analog-3931362__480.jpg";
+                } else {
+                  images = images.original;
+                }
                 return (
                   <div key={key}>
-                    <img src={data} alt="" />
+                    <img src={images} alt="" />
                   </div>
                 );
               })}
             </Carousel>
           </Grid.Column>
-          <Grid.Column width={4}>
+
+          <Grid.Column width={12}>
             <Input
               icon={{ name: "search", circular: true, link: true }}
               placeholder="Search..."
+              onChange={e => {
+                this.pencarianActor(e);
+              }}
             />
-
             <Grid style={{ marginTop: 20 }}>
-              <Grid.Column>
-                <Card
-                  image="https://cdn.pixabay.com/photo/2015/05/15/09/13/demonstration-767982__480.jpg"
-                  header="Contoh Nama"
-                  meta="Status"
-                  description="description"
-                  extra={
-                    <Icon name="star">
-                      <p>"user"</p>
-                    </Icon>
-                  }
-                />
-              </Grid.Column>
+              {this.state.dataActor.map((data, key) => {
+                var images = { ...data.person.image };
+
+                if (data.person.image === null) {
+                  images =
+                    "https://cdn.pixabay.com/photo/2019/01/13/21/36/analog-3931362__480.jpg";
+                } else {
+                  images = images.original;
+                }
+
+                return (
+                  <Grid.Column width={4}>
+                    <Card
+                      image={images}
+                      header={data.person.name}
+                      meta={data.person.gender}
+                      extra={
+                        <Icon name="star">
+                          <p>{data.score}</p>
+                        </Icon>
+                      }
+                    />
+                  </Grid.Column>
+                );
+              })}
             </Grid>
           </Grid.Column>
         </Grid>
